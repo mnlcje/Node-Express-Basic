90b6f15bd7881e82b86c35be60c0535dc4f04bfc
mnkcje <mnkcje2050@gmail.com>
2019-10-03 14:42:46 +0530

    Create api.ts

---
+++ Src/api.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import * as http  from 'http';
+
+const api = express();
+
+api.use('/',(req, res, next) => {
+
+    console.log('In the middleware!');
+   
+    // Allows the request to continue to the next middleware in line
+    next();
+});
+
+api.use((req, res, next) => {
+    
+    console.log('In another middleware!');
+
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(api);
+server.listen(5000);
+
+
+
